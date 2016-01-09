e3a9d7fdf57af4f614a87498e3afeaf00cd89e20
wi1d5ky <poppin.wildsky@gmail.com>
2016-01-09 22:35:32 +0800

    新增存檔功能

--- app/src/main/java/cc/wildsky/counterdays/AddCounterActivity.java
+++ app/src/main/java/cc/wildsky/counterdays/AddCounterActivity.java
@@ -1,15 +1,27 @@
 package cc.wildsky.counterdays;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,18 +29,21 @@ import java.util.Date;
 public class AddCounterActivity extends AppCompatActivity implements View.OnClickListener {
 
     String eventDate;
+    private String SAVE_FILE_NAME = "counter_savefile1.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_counter);
+
         Button b = (Button) findViewById(R.id.button);
         b.setOnClickListener(this);
 
         CalendarView ct = (CalendarView) findViewById(R.id.calView);
-        ct.setOnDateChangeListener(new CalendarView.OnDateChangeListener() { //監聽當日期改變
-
+        ct.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            //監聽日期改變
             public void onSelectedDayChange(CalendarView view, int year, int month, int date) { //Month從0算起
-                eventDate = year + "/" + (month > 8 ? "" : "0") + (month + 1) + "/" + (date > 10 ? "" : "0") + date;
+                eventDate = year + "/" + (month > 8 ? "" : "0") + (month + 1) + "/" + (date >= 10 ? "" : "0") + date;
                 Toast.makeText(getApplicationContext(), eventDate, Toast.LENGTH_SHORT).show();
             }
         });
@@ -37,17 +52,55 @@ public class AddCounterActivity extends AppCompatActivity implements View.OnClic
     long between(String small, String big, SimpleDateFormat sdf) {
         long fdate = 0, ndate = 0;
         try {
-            try {
-                fdate = sdf.parse(small).getTime();
-                ndate = sdf.parse(big).getTime();
-            } catch (java.lang.NullPointerException e) {
-                Toast.makeText(getApplicationContext(), "Something Wrong!!", Toast.LENGTH_LONG).show();
-            }
+            fdate = sdf.parse(small).getTime();
+            ndate = sdf.parse(big).getTime();
+        } catch (java.lang.NullPointerException e) {
+            Log.e("NullPtr", e.toString());
         } catch (ParseException e) {
-            Toast.makeText(getApplicationContext(), "Something Wrong!!", Toast.LENGTH_LONG).show();
+            Log.e("ParseEcp", e.toString());
+        }
+        return (fdate - ndate) / (24 * 60 * 60 * 1000);
+    }
+
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(SAVE_FILE_NAME, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private JSONArray readFromFile() {
+        JSONArray counterArray = new JSONArray();
+
+        try {
+            InputStream inputStream = openFileInput(SAVE_FILE_NAME);
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                String before_conv = stringBuilder.toString();
+                counterArray = new JSONArray(before_conv);
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("File not found", e.toString());
+        } catch (IOException e) {
+            Log.e("Can not read file", e.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        return (fdate - ndate) / (24 * 60 * 60 * 1000);
+        return counterArray;
     }
 
     @Override
@@ -59,16 +112,31 @@ public class AddCounterActivity extends AppCompatActivity implements View.OnClic
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
         String today = formatter.format(new Date());
 
+        String string = "距離" + evenName + "還剩下" + between(eventDate, today, formatter) + "天";
+
+        JSONArray dataArray = readFromFile();
+
+        try {
+            JSONObject newEvent = new JSONObject();
+            newEvent.put("Name", evenName);
+            newEvent.put("Val", between(eventDate, today, formatter));
+            dataArray.put(newEvent);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        writeToFile(dataArray.toString());
+
         Intent resultData = new Intent();
-        resultData.putExtra("valueName", "距離" + evenName + "還剩下" + between(eventDate, today, formatter) + "天");
+        resultData.putExtra("response", string);
         setResult(Activity.RESULT_OK, resultData);
         finish();
     }
 
     public void cancel(View v) {
-        Toast.makeText(getApplicationContext(), "取消囉～", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "已取消！", Toast.LENGTH_SHORT).show();
         Intent resultData = new Intent();
-        resultData.putExtra("valueName", "cancel");
+        resultData.putExtra("response", "cancel");
         setResult(Activity.RESULT_OK, resultData);
         finish();
     }
--- app/src/main/java/cc/wildsky/counterdays/MainActivity.java
+++ app/src/main/java/cc/wildsky/counterdays/MainActivity.java
@@ -1,37 +1,94 @@
 package cc.wildsky.counterdays;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import org.json.JSONArray;
+import org.json.JSONException;
 
-import static android.widget.PopupMenu.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity implements View.OnLongClickListener, View.OnClickListener {
 
-    static int request_code = 123;
+    final static int add_code = 123;   // add a new counter
+    final static int edit_code = 333;  // edit a counter
     static int counterNum = 0;
-    int a = 0;
     TextView selectedView = null;
+    private String SAVE_FILE_NAME = "counter_savefile1.json";
+
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput(SAVE_FILE_NAME, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private JSONArray readFromFile() {
+
+        JSONArray jarray = new JSONArray();
+
+        try {
+            InputStream inputStream = openFileInput(SAVE_FILE_NAME);
+
+            if (inputStream != null) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((receiveString = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                String before_conv = stringBuilder.toString();
+                jarray = new JSONArray(before_conv);
+            }
+        } catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jarray;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        JSONArray data = readFromFile();
+
+        for (int i = 0; i < data.length(); i++) {
+            try {
+                addCounter("距離" + data.getJSONObject(i).getString("Name") +
+                        "還剩下" + data.getJSONObject(i).getInt("Val") + "天");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new FloatingActionButton.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 go(v);
@@ -43,12 +100,37 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
     public void onClick(View v) {
         selectedView = (TextView) v;
         Intent it = new Intent(this, AddCounterActivity.class);
-        startActivityForResult(it, 333);
+        startActivityForResult(it, edit_code);
+    }
+
+    private void removeEleFromFile(String s) {
+        JSONArray origin_data = readFromFile();
+        JSONArray new_data = new JSONArray();
+        boolean deleted = false;
+
+        if (origin_data != null) {
+            try {
+                for (int i = 0; i < origin_data.length(); i++) {
+                    String str = "距離" + origin_data.getJSONObject(i).getString("Name") +
+                            "還剩下" + origin_data.getJSONObject(i).getInt("Val") + "天";
+                    if (!str.equals(s) && !deleted)
+                        new_data.put(origin_data.get(i));
+                    else
+                        deleted = true;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        writeToFile(new_data.toString());
     }
 
     @Override
     public boolean onLongClick(View v) {
         LinearLayout LL = (LinearLayout) findViewById(R.id.LL);
+        TextView tv = (TextView) v;
+        removeEleFromFile(tv.getText().toString());
+
         LL.removeView(v);
         Toast.makeText(getApplicationContext(), "已移除", Toast.LENGTH_SHORT).show();
         return true;
@@ -56,25 +138,26 @@ public class MainActivity extends AppCompatActivity implements View.OnLongClickL
 
     public void go(View v) {
         Intent it = new Intent(this, AddCounterActivity.class);
-        startActivityForResult(it, request_code);
+        startActivityForResult(it, add_code);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode,
                                     Intent data) {
-        if (requestCode == request_code && resultCode == RESULT_OK) {
-            String myValue = data.getStringExtra("valueName");
-            if (!myValue.equals("cancel"))
-                addCounter(myValue);
-        }
-
-        if (requestCode == 333 && resultCode == RESULT_OK) {
-            String myValue = data.getStringExtra("valueName");
-            if (!myValue.equals("cancel")) {
-                LinearLayout LL = (LinearLayout) findViewById(R.id.LL);
-                LL.removeView(selectedView);
-                Toast.makeText(getApplicationContext(), "已更新", Toast.LENGTH_SHORT).show();
-                addCounter(myValue);
+        if (resultCode == RESULT_OK) {
+            String myValue = data.getStringExtra("response");
+            switch (requestCode) {
+                case edit_code:
+                    if (!myValue.equals("cancel")) {
+                        LinearLayout LL = (LinearLayout) findViewById(R.id.LL);
+                        LL.removeView(selectedView);
+                        removeEleFromFile(selectedView.getText().toString());
+                        Toast.makeText(getApplicationContext(), "已更新", Toast.LENGTH_SHORT).show();
+                    }
+                case add_code:
+                    if (!myValue.equals("cancel"))
+                        addCounter(myValue);
+                    break;
             }
         }
     }
